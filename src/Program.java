9dd35016f8ba8545b4ea83b82155b09780443f33
abhis <abhishekkushwaha5169@gmail.com>
2025-05-26 19:24:41 +0530

    first program

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,5 @@
 public class Program {
     public static void main(String[] args) {
-        System.out.println("raushan kummar"+" Abhishek kumar");
+        System.out.println("Raushan kummar"+" Abhishek kumar");
     }
 }
